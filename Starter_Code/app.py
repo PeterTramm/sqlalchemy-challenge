a660c389cb97e69a35c4a999dffcc494f8acbfae
peter.extra <peter.extra@outlook.com>
2023-11-09 11:57:54 +1100

    removed uncessary modules

--- Starter_Code/app.py
+++ Starter_Code/app.py
@@ -1,12 +1,9 @@
-import numpy as np
-
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
-import numpy as np
-import pandas as pd
+
 import datetime as dt
 
 from flask import Flask, jsonify
